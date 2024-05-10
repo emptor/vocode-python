ca4cf15896255d908727e5eda549e03d253b3f89
Gabriel Puliatti <gabriel@emptor.io>
2024-05-10 00:20:50 -0500

    Update factory.py

--- vocode/streaming/transcriber/factory.py
+++ vocode/streaming/transcriber/factory.py
@@ -1,12 +1,12 @@
 import logging
-from typing import Optional
-import typing
+from typing import Optional, Dict, Type
 from vocode.streaming.models.transcriber import (
     AssemblyAITranscriberConfig,
     AzureTranscriberConfig,
     DeepgramTranscriberConfig,
     GoogleTranscriberConfig,
     RevAITranscriberConfig,
+    GladiaTranscriberConfig,
     TranscriberConfig,
     TranscriberType,
 )
@@ -15,23 +15,26 @@ from vocode.streaming.transcriber.deepgram_transcriber import DeepgramTranscribe
 from vocode.streaming.transcriber.google_transcriber import GoogleTranscriber
 from vocode.streaming.transcriber.rev_ai_transcriber import RevAITranscriber
 from vocode.streaming.transcriber.azure_transcriber import AzureTranscriber
-
+from vocode.streaming.transcriber.gladia_transcriber import GladiaTranscriber
 
 class TranscriberFactory:
+    def __init__(self):
+        self.transcriber_map: Dict[Type[TranscriberConfig], Type[TranscriberBase]] = {
+            DeepgramTranscriberConfig: DeepgramTranscriber,
+            GoogleTranscriberConfig: GoogleTranscriber,
+            AssemblyAITranscriberConfig: AssemblyAITranscriber,
+            RevAITranscriberConfig: RevAITranscriber,
+            AzureTranscriberConfig: AzureTranscriber,
+            GladiaTranscriberConfig: GladiaTranscriber,
+        }
+
     def create_transcriber(
         self,
         transcriber_config: TranscriberConfig,
         logger: Optional[logging.Logger] = None,
     ):
-        if isinstance(transcriber_config, DeepgramTranscriberConfig):
-            return DeepgramTranscriber(transcriber_config, logger=logger)
-        elif isinstance(transcriber_config, GoogleTranscriberConfig):
-            return GoogleTranscriber(transcriber_config, logger=logger)
-        elif isinstance(transcriber_config, AssemblyAITranscriberConfig):
-            return AssemblyAITranscriber(transcriber_config, logger=logger)
-        elif isinstance(transcriber_config, RevAITranscriberConfig):
-            return RevAITranscriber(transcriber_config, logger=logger)
-        elif isinstance(transcriber_config, AzureTranscriberConfig):
-            return AzureTranscriber(transcriber_config, logger=logger)
+        transcriber_class = self.transcriber_map.get(type(transcriber_config))
+        if transcriber_class:
+            return transcriber_class(transcriber_config, logger=logger)
         else:
             raise Exception("Invalid transcriber config")
